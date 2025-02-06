5e4cb83c675fb803ed0eb4cff9312126512ac11b
Rishi kashyap <kashyap1strishi@gmail.com>
2025-02-06 12:06:39 +0530

    bug-2 resolved

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,4 @@
-import React, {  } from "react";
+import React, { useEffect, useState } from "react";
 import Builder from "./builder";
 import Loder from './Loder'
 import Home_first from "./Home/Home_first";
@@ -7,6 +7,27 @@ import Navbar from "./Navbar/Navbar";
 import Footer from "./Footer/Footer";
 
 const ResumeBuilder = () => {
+  const [token, setToken] = useState();
+  useEffect(() => {
+    // Extract the token directly from the URL
+    const url = window.location.href;
+    const tokenFromUrl = url.split("/?tokenbyurl=")[1]; // Gets the token part after `/?`
+    console.log(tokenFromUrl);
+    if (tokenFromUrl) {
+      // Save token to localStorage and state
+      localStorage.setItem("token", tokenFromUrl);
+      setToken(tokenFromUrl);
+    } else if (typeof window !== "undefined") {
+      // Retrieve token from localStorage if not found in URL
+      const storedToken = localStorage.getItem("token");
+      setToken(storedToken);
+    }
+  }, []);
+
+  if (!token) {
+    return null; // Exit if no token
+  }
+  console.log(token);
   return (
     <>
     {/* <Builder /> */}
