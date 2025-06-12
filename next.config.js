e5c0371716f86b8fd138f75f6725226843a860a1
Rishi kashyap <kashyap1strishi@gmail.com>
2025-06-12 19:08:37 +0530

    Update next.config.js to set basePath and enable trailingSlash

--- next.config.js
+++ next.config.js
@@ -1,15 +1,32 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   images: {
+//     domains: ['avatars.githubusercontent.com'],
+//     remotePatterns:[{
+//       protocol:'https',
+//       hostname: 'novajobs.us'
+//     }]
+//   }
+// }
+
+// module.exports = nextConfig
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: '/airesume',
+  trailingSlash: true,
   images: {
     domains: ['avatars.githubusercontent.com'],
-    remotePatterns:[{
-      protocol:'https',
-      hostname: 'novajobs.us'
-    }]
-  }
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'novajobs.us',
+      },
+    ],
+  },
 }
 
-module.exports = nextConfig
-
-
+module.exports = nextConfig;
