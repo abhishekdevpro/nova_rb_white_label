49fc5443c9b416ab8fe6a1088d57da5c5c5e3138
Rishi kashyap <kashyap1strishi@gmail.com>
2025-06-13 12:34:21 +0530

    Update image configuration and enhance Home_first component

--- next.config.js
+++ next.config.js
@@ -13,17 +13,36 @@
 // module.exports = nextConfig
 
 
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   basePath: '/airesume',
+//   trailingSlash: true,
+//   images: {
+//     domains: ['avatars.githubusercontent.com'],
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'novajobs.us',
+//       },
+//     ],
+//   },
+// }
+
+// module.exports = nextConfig;
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   basePath: '/airesume',
   trailingSlash: true,
   images: {
-    domains: ['avatars.githubusercontent.com'],
+    domains: ['avatars.githubusercontent.com', 'airesume.novajobs.us'], // ✅ added your image domain
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'novajobs.us',
+        hostname: 'airesume.novajobs.us', // ✅ fix here
       },
     ],
   },
